0cf404983e40eb0b30930f2821cb84fb69737802
Piêtro Braga Aquino Júnior <pietrojunior@aluno.uema.br>
2021-03-03 23:37:54 -0300

    Update DashboardCovidApplication.java

--- dashboard_covid/src/main/java/com/example/dashboard_covid/DashboardCovidApplication.java
+++ dashboard_covid/src/main/java/com/example/dashboard_covid/DashboardCovidApplication.java
@@ -1,5 +1,7 @@
 package com.example.dashboard_covid;
 
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -7,9 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class DashboardCovidApplication {
 	private static final Logger log = LoggerFactory.getLogger(DashboardCovidApplication.class);
 	public static void main(String[] args) {
@@ -25,5 +30,9 @@ public class DashboardCovidApplication {
 			Status status = restTemplate.getForObject("https://covid19-api.org/api/status", Status.class);
 			log.info(status.toString());
 		};
+	}
+	@GetMapping("/")
+	String inicio(){
+	  return "Bem-vindo!";
 	}
 }
