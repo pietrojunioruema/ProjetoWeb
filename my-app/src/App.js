271a783a9dd0cfc944e0fa692c90e85c7aadb54a
Piêtro Braga Aquino Júnior <pietrojunior@aluno.uema.br>
2021-03-04 22:48:14 -0300

    Colocando um gráfico finalmente

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import api from './api';
 import './App.css';
+import { Doughnut } from 'react-chartjs-2';
 class App extends Component {
   state = {
     status: [],
@@ -12,8 +13,28 @@ class App extends Component {
   }
   render() {
     const { status } = this.state;
+    const data = {
+      labels: ['Red', 'Green', 'Yellow'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+        }
+      ]
+    };
+
+
+
     return (
     <div>
+
+  <div className="flex flex-col items-center w-full max-w-md">
+				<h2>Doughnut Example</h2>
+				<Doughnut data={data} />
+			</div>
+
+
 <ul>
   <li><a class="active" href="#home">Home</a></li>
   <li><a href="#news">News</a></li>
